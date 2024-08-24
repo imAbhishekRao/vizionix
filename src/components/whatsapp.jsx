bf031cc0f1fe910a75582eaf776434e8e95af4b0
Abhishek Gobind Rao <abhishek.rao.4200@gmail.com>
2024-08-24 14:03:05 +0530

    icon added

--- src/components/whatsapp.jsx
+++ src/components/whatsapp.jsx
@@ -5,7 +5,7 @@ const FixedWhatsAppButton = () => {
   return (
     <a
       aria-label="Chat on WhatsApp"
-      href="https://wa.me/097808 83345?text=Hello%20Vizionix!%20I%27m%20interested%20in%20your%20marketing%20and%20web%20development%20services.%20Could%20you%20please%20provide%20more%20information%3F" // Replace with your WhatsApp number and desired prefilled text
+      href="https://wa.me/+9197808 83345?text=Hello%20Vizionix!%20I%27m%20interested%20in%20your%20marketing%20and%20web%20development%20services.%20Could%20you%20please%20provide%20more%20information%3F" // Replace with your WhatsApp number and desired prefilled text
       target="_blank" // Opens the link in a new tab
       rel="noopener noreferrer" // For security reasons
       style={styles.button}
