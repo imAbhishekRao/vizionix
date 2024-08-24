4f0954d0833486a9503a60b7edc18ea3ccbfc95f
Abhishek Gobind Rao <abhishek.rao.4200@gmail.com>
2024-08-24 14:17:03 +0530

    finally

--- src/components/whatsapp.jsx
+++ src/components/whatsapp.jsx
@@ -5,7 +5,7 @@ const FixedWhatsAppButton = () => {
   return (
     <a
       aria-label="Chat on WhatsApp"
-      href="https://wa.me/+9197808 83345?text=Hello%20Vizionix!%20I%27m%20interested%20in%20your%20marketing%20and%20web%20development%20services.%20Could%20you%20please%20provide%20more%20information%3F" // Replace with your WhatsApp number and desired prefilled text
+      href="https://wa.me/+919780883345?text=Hello%20Vizionix!%20I%27m%20interested%20in%20your%20marketing%20and%20web%20development%20services.%20Could%20you%20please%20provide%20more%20information%3F" // Prefilled text link
       target="_blank" // Opens the link in a new tab
       rel="noopener noreferrer" // For security reasons
       style={styles.button}
@@ -27,8 +27,14 @@ const styles = {
     zIndex: 1000, // Ensures it's above other content
   },
   image: {
-    width: '60px', // Adjust the size as needed
-    height: '60px', // Adjust the size as needed
+    width: '60px', // Default size for larger screens
+    height: '60px',
+  },
+  '@media (max-width: 768px)': { // Media query for mobile devices
+    image: {
+      width: '40px', // Reduced size for smaller screens
+      height: '40px',
+    },
   },
 };
 
